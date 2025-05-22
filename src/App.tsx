cf4a77d9f9444fde3e1a6908c69a035a1b0ab899
Daihua Ye <93221806+dd-daihuaye@users.noreply.github.com>
2025-05-22 12:14:54 -0700

    Add simple carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './Carousel';
 
 function App() {
+  const items = [
+    'Card 1',
+    'Card 2',
+    'Card 3',
+    'Card 4',
+    'Card 5',
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel items={items.map((text) => <div>{text}</div>)} />
     </div>
   );
 }
